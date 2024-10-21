da308f1f4bebfaab370ce88797ec492de9ff2bb7
JacobsWout <r0930151@student.thomasmore.be>
2024-10-21 15:54:07 +0200

    push van lid

---
+++ lid-service/src/main/java/fact/it/lidservice/LidServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.lidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LidServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LidServiceApplication.class, args);
+    }
+
+}
