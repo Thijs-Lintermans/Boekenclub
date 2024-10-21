06d4f6eef3bf3b2dfe8aebaa2a52b0a1a5f4cff5
ThijsLintermans <thijslintermans2004@gmail.com>
2024-10-21 15:06:18 +0200

    project

---
+++ boek-service/src/main/java/fact/it/boekservice/BoekServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.boekservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoekServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoekServiceApplication.class, args);
+	}
+
+}
